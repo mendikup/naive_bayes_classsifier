522f12a665ce0862723630ae7140150a84ee8a64
mendi kuperman <mendikuperman@gmail.com>
2025-07-10 16:23:16 +0300

    add functions to handle with user options

--- ui/menu.py
+++ ui/menu.py
@@ -1,3 +1,5 @@
+from random import choice
+
 
 class Menu:
     @staticmethod
@@ -7,6 +9,55 @@ class Menu:
                       "2. copy URL for a csv file\n"
                       "3. Analyze by bayesian model statistics")
         return choise
+
+
+    @staticmethod
+    def suggest_options(options:list) -> str:
+        choices = {}
+        print("please select an option from the option below:")
+
+        i = 1
+
+        for option in options:
+            choices[i] = option
+            print(f"{i}. {option}")
+            i+=1
+
+        choice = input()
+
+        if not choice.isdigit():
+            print("enter numbers only")
+            return Menu.suggest_options(options)
+
+        int_choice=int(choice)
+
+        if not (0 < int_choice < i):
+            print(f"enter a number between 1 and {i-1}")
+            return Menu.suggest_options(options)
+
+        return choices[int_choice]
+
+
+    @staticmethod
+    def get_parmas(suggestions:dict) ->str:
+
+        """create  a dictionary to save each option(number the user choose) as a key and the
+         parameters in values so we can store the options as a dictionary so we can send
+         it to the bayes_model
+        """
+        selected_options = {}
+
+        for suggestion in suggestions:
+            print(f"for the parameter{suggestion}:")
+            selected_chois=Menu.suggest_options(suggestion)
+            selected_options[suggestions]=selected_chois
+
+        return selected_options
+
+
+
+
+
 
 
 
