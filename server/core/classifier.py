91175e53054c0c490ed5a1d871c7ef80d923c761
mendi kuperman <mendikuperman@gmail.com>
2025-07-14 12:55:32 +0300

    changed to clearer variable nams ,added utils function

--- server/core/classifier.py
+++ server/core/classifier.py
@@ -12,31 +12,28 @@ class Classifier:
         :return: Most probable class label.
         """
 
-        # Compute posterior probability for each class using conditional probabilities
+        # Compute posterior probability for each label using conditional probabilities
         final_result = {}
-        for option in model["sum"]:
-            list_of_nums = []
-            if not option == "total_cases":
-                for column in dict_test:
-                    try:
-                        conditional_prob = model[option][column][dict_test[column]]
-                    except KeyError:
-                        conditional_prob = 1e-10  #if the specific value is
-
-                    list_of_nums.append(conditional_prob)
-                list_of_nums.append(model['sum'][option] / model['sum']['total_cases'])
-                list_of_nums = np.array(list_of_nums)
+        for label in model["sum"]:
+            likelihoods = []
+            if label == "total_cases":
+                continue
 
 
+            for feature ,value in dict_test.items():
+                try:
+                    conditional_prob = model[label][feature][dict_test[value]]
+                except KeyError:
+                    conditional_prob = 1e-10  #if the specific value is missing we add min value
 
-                # multiply the
-                result = np.prod(list_of_nums)
-
-                final_result[option] = result
+                likelihoods.append(conditional_prob)
+            likelihoods.append(model['sum'][label] / model['sum']['total_cases'])
+            likelihoods = np.array(likelihoods)
+            result = np.prod(likelihoods)
+            final_result[label] = result
 
 
         most_prob = max(final_result, key=final_result.get)
-        # print(f" according to the model the answer to your question in the  highest probability is {strong} ")
 
         return most_prob
 
