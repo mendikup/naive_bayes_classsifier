0e40169f9ec270f4d8def287d05583f4df4c7b9f
mendi kuperman <mendikuperman@gmail.com>
2025-07-19 23:10:35 +0300

    change some things in suggest_option function

--- server/app/endpoints.py
+++ server/app/endpoints.py
@@ -1,6 +1,4 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
-
 from server.core.dal.dal import Dal
 import pandas as pd
 from server.core.naive_bayes_trainer import Naive_bayesian_trainer
