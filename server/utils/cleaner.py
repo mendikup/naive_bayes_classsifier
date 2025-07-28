2bc8cfc927c539c9c7c33a9319e4f6e9d0a92dc7
mendi kuperman <mendikuperman@gmail.com>
2025-07-28 12:20:15 +0300

    added documentation

--- server/utils/cleaner.py
+++ server/utils/cleaner.py
@@ -7,11 +7,11 @@ class Cleaner:
     @staticmethod
     def ensure_there_is_no_nan(df):
         """
-            Cleans the DataFrame by dropping rows or filling in missing values.
+        Cleans the DataFrame by dropping rows or filling in missing values.
 
-            :param df: DataFrame possibly containing NaN.
-            :return: Cleaned DataFrame.
-            """
+        :param df: DataFrame possibly containing NaN.
+        :return: Cleaned DataFrame.
+        """
         df=Cleaner.drop_columns_with_nan_above_threshold(df, threshold=0.6)
         df= Cleaner.drop_empty_rows(df)
         return df
@@ -36,10 +36,10 @@ class Cleaner:
     @staticmethod
     def drop_requested_columns(df, requested_columns):
         """
-           Removes selected columns from the DataFrame.
+        Removes selected columns from the DataFrame.
 
-           :param df: DataFrame.
-           :param requested_columns: List of column names to drop.
-           """
+        :param df: DataFrame.
+        :param requested_columns: List of column names to drop.
+        """
 
         return  df.drop(columns=requested_columns).copy()
